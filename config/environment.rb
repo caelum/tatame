1515d2a43d72953d32c13a1d55fbd1247c064606
Caue Guerra <caue@Caue.local>
2009-01-13 14:05:52 -0200

    configuring

--- config/environment.rb
+++ config/environment.rb
@@ -23,10 +23,14 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on. 
   # They can then be installed with "rake gems:install" on new installations.
   # You have to specify the :lib option for libraries, where the Gem name (sqlite3-ruby) differs from the file itself (sqlite3)
-  # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-  # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
+  config.gem "aslakhellesoy-webrat", :source => "http://gems.github.com"
+  config.gem "rspec"
+  config.gem "rspec-rails"
+  config.gem "mysql"
+  config.gem "cucumber"
+  config.gem "sqlite3-ruby", :lib => "sqlite3"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
