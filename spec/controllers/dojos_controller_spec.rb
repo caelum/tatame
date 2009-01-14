8f01c03ff2a5fcea8b5d2bd5bf2e095fb3dcd557
Fabio Kung <fabio.kung@gmail.com>
2009-01-14 08:39:54 -0200

    fixing mock behavior

--- spec/controllers/dojos_controller_spec.rb
+++ spec/controllers/dojos_controller_spec.rb
@@ -4,15 +4,16 @@ describe DojosController do
   
   before(:each) do
     @dojo = mock_model(Dojo)
-    @dojo.stub!(:new_record?).and_return(false)
+    @dojo.stub!(:new_record?).and_return(true)
   end
   
   it "should save the dojo and redirect to index" do
     Dojo.should_receive(:new).and_return(@dojo)
-    @dojo.should_receive(:save).and_return(false)
+    @dojo.should_receive(:save).and_return(true)
     
     post 'create'
     assigns[:dojo].should equal(@dojo)
     response.should redirect_to(:action => 'index')
   end
+  
 end
