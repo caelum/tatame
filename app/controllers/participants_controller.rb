bd0c744c8de82cfad4b3fc40a87d4ec9157f6d85
Luiz Fernando <luiz@notebook.(none)>
2009-03-04 16:46:12 -0300

    Blocking the participants list also in the controller

--- app/controllers/participants_controller.rb
+++ app/controllers/participants_controller.rb
@@ -1,11 +1,16 @@
 class ParticipantsController < ApplicationController
   def create
-    @participant = Participant.new(params[:participant])
-    @participant.dojo_id = params[:dojo_id]
-    if @participant.save
-      flash[:notice] = "Successfully added participant #{@participant.name}"
+    dojo = Dojo.find params[:dojo_id]
+    if dojo.block_list?
+      flash[:notice] = "The dojo's participant list is blocked"
     else
-    flash[:notice] = "Empty name"
+      @participant = Participant.new(params[:participant])
+      @participant.dojo_id = params[:dojo_id]
+      if @participant.save
+        flash[:notice] = "Successfully added participant #{@participant.name}"
+      else
+      flash[:notice] = "Empty name"
+      end
     end
     redirect_url = request.env['HTTP_REFERER'] || root_url
     redirect_to redirect_url
@@ -13,8 +18,10 @@ class ParticipantsController < ApplicationController
   
   def destroy
     @participant = Participant.find(params[:id])
-    @participant.destroy
-    flash[:notice] = "Successfully removed participant #{@participant.name}"
+    unless @participant.dojo.block_list?
+      @participant.destroy
+      flash[:notice] = "Successfully removed participant #{@participant.name}"
+    end
     redirect_to root_url
   end
 end
