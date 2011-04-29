023607a0199f491e3c0682340b6c87b3e03a29f1
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2011-04-29 16:51:58 -0300

    Integration server build script

---
+++ hudson.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+source ~/.profile
+source ~/.bashrc
+source ~/.rvm/scripts/rvm
+
+rvm --create 1.8.7-p302@tatame
+rvm gemset import tatame.gems
+cp ./config/database.yml.example ./config/database.yml
+rake db:create RAILS_ENV=test
+
+rake spec features
