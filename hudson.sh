9ffa3f713f8f2c01f7ccd3e13b2d5982f8ffc67f
Luiz Fernando Oliveira Corte Real <sr.saude@gmail.com>
2011-04-29 17:29:58 -0300

    Fixing Hudson build script (missing db:migrate)

--- hudson.sh
+++ hudson.sh
@@ -8,5 +8,6 @@ rvm --create 1.8.7-p302@tatame
 rvm gemset import tatame.gems
 cp ./config/database.yml.example ./config/database.yml
 rake db:create RAILS_ENV=test
+rake db:migrate RAILS_ENV=test
 
 rake spec features RAILS_ENV=test
