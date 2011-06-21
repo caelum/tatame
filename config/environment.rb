7a8f9290cc62fd4084b45d84a23f2cdcecf05080
Felipe Taiarol <felipebtaiarol@gmail.com>
2011-06-20 22:53:48 -0700

    On branch master  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   Rakefile
    	modified:   app/models/dojo.rb
    	modified:   app/views/dojos/new.html.erb
    	modified:   app/views/layouts/application.html.erb
    	modified:   config/boot.rb
    	modified:   config/environment.rb
    	modified:   config/routes.rb
    	modified:   script/server
    
     Untracked files:
       (use "git add <file>..." to include in what will be committed)
    
    	app/controllers/graphics_controller.rb
    	app/views/graphics/
    	db/migrate/20110621042243_add_programming_language_to_dojos.rb

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,7 @@
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 # former used version: 2.2.2
 
+require 'thread'
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
