159ac2d1f5a8663ee6ed39c2b1f663b81281790e
Felipe Taiarol <felipebtaiarol@gmail.com>
2011-06-27 17:43:21 -0700

    modified:   app/controllers/graphics_controller.rb 	modified:   app/views/graphics/index.html.erb 	modified:   app/views/layouts/application.html.erb

--- app/controllers/graphics_controller.rb
+++ app/controllers/graphics_controller.rb
@@ -4,17 +4,7 @@ require 'gruff'
 class GraphicsController < ApplicationController 
   
   def index 
+    @programming_languages = Dojo.programming_languages
   end
 
-  def generate_image
-    g = Gruff::Bar.new 
-    g.title = "Estatistics"
-
-    Dojo.programming_languages.each do |l|
-      g.data(l[0], l[1])
-    end
-
-   g.minimum_value = 0
-   send_data(g.to_blob, :disposition => 'inline', :type => 'image/png', :filename => "arbitaryfilename.png")
-  end
 end
